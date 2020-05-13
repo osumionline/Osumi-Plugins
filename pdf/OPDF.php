dc36a2e8f7a47d592705521d05c3c12d09ac873f
igorosabel <inigo.gorosabel@gmail.com>
2020-05-13 21:56:49 +0200

    pdf - 1.1.0 - Añado tipado fuerte

--- pdf/OPDF.php
+++ pdf/OPDF.php
@@ -1,16 +1,21 @@
-<?php
+<?php declare(strict_types=1);
 class OPDF {
-	private $creator  = '';
-	private $author   = '';
-	private $title    = '';
-	private $subject  = '';
-	private $keywords = '';
-	private $font     = 'helvetica';
-	private $pages    = [];
-	private $pdf_obj  = null;
-	private $pdf_dir  = '';
+	private string $creator  = '';
+	private string $author   = '';
+	private string $title    = '';
+	private string $subject  = '';
+	private string $keywords = '';
+	private string $font     = 'helvetica';
+	private array  $pages    = [];
+	private ?TCPDF $pdf_obj  = null;
+	private string $pdf_dir  = '';
 
-	function __construct($data=null) {
+	/**
+	 * Set up PDF file information
+	 *
+	 * @param array Information of the PDF file
+	 */
+	function __construct(array $data=null) {
 		if (!is_null($data) && is_array($data)) {
 			$this->setCreator(  array_key_exists('creator',  $data)?$data['creator']  : '' );
 			$this->setAuthor(   array_key_exists('author',   $data)?$data['author']   : '' );
@@ -22,83 +27,203 @@ class OPDF {
 		}
 	}
 
-	public function setCreator($c) {
+	/**
+	 * Set the creator name of the PDF file
+	 *
+	 * @param string $c Name of the creator
+	 *
+	 * @return void
+	 */
+	public function setCreator(string $c): void {
 		$this->creator = $c;
 	}
 
-	public function getCreator() {
+	/**
+	 * Get the PDF files creators name
+	 *
+	 * @return string Name of the creator
+	 */
+	public function getCreator(): string {
 		return $this->creator;
 	}
 
-	public function setAuthor($a) {
+	/**
+	 * Set the PDF files author name
+	 *
+	 * @param string $a Name of the author
+	 *
+	 * @return void
+	 */
+	public function setAuthor(string $a): void {
 		$this->author = $a;
 	}
 
+	/**
+	 * Get the PDF files authors name
+	 *
+	 * @return string Name of the author
+	 */
 	public function getAuthor() {
 		return $this->author;
 	}
 
-	public function setTitle($t) {
+	/**
+	 * Set PDF files title
+	 *
+	 * @param string $t Title of the file
+	 *
+	 * @return void
+	 */
+	public function setTitle(string $t): void {
 		$this->title = $t;
 	}
 
-	public function getTitle() {
+	/**
+	 * Get PDF files title
+	 *
+	 * @return string Title of the file
+	 */
+	public function getTitle(): string {
 		return $this->title;
 	}
 
-	public function setSubject($s) {
+	/**
+	 * Set PDF files subject
+	 *
+	 * @param string $s Subject of the file
+	 *
+	 * @return void
+	 */
+	public function setSubject(string $s): void {
 		$this->subject = $s;
 	}
 
-	public function getSubject() {
+	/**
+	 * Get PDF files subject
+	 *
+	 * @return string Subject of the file
+	 */
+	public function getSubject(): string {
 		return $this->subject;
 	}
 
-	public function setKeywords($k) {
+	/**
+	 * Set PDF files keyword list
+	 *
+	 * @param string $k String of comma separated keywords
+	 *
+	 * @return void
+	 */
+	public function setKeywords(string $k): void {
 		$this->keywords = $k;
 	}
 
-	public function getKeywords() {
+	/**
+	 * Get PDF files keyword list
+	 *
+	 * @return string List of files keywords
+	 */
+	public function getKeywords(): string {
 		return $this->keywords;
 	}
 
-	public function setFont($f) {
+	/**
+	 * Set PDF files font family
+	 *
+	 * @param string $f Name of the font to be used in the file
+	 *
+	 * @return void
+	 */
+	public function setFont(string $f): void {
 		$this->font = $f;
 	}
 
-	public function getFont() {
+	/**
+	 * Get the font family used in the PDF file
+	 *
+	 * @return string Name of the font to be used in the file
+	 */
+	public function getFont(): string {
 		return $this->font;
 	}
 
-	public function setPages($p) {
+	/**
+	 * Set array of pages of the PDF file
+	 *
+	 * @param array $p List of pages
+	 *
+	 * @return void
+	 */
+	public function setPages(array $p): void {
 		$this->pages = $p;
 	}
 
-	public function getPages() {
+	/**
+	 * Get array of pages of the PDF file
+	 *
+	 * @return array List of pages
+	 */
+	public function getPages(): array {
 		return $this->pages;
 	}
 
-	public function addPage($p) {
+	/**
+	 * Add a new page to the PDF file in HTML format
+	 *
+	 * @param string $p New page to be added
+	 *
+	 * @return void
+	 */
+	public function addPage(string $p): void {
 		array_push($this->pages, $p);
 	}
 
-	public function setPdfObj($po) {
+	/**
+	 * Set the PDF resource object of the generated PDF
+	 *
+	 * @param resource $po PDF resource object
+	 *
+	 * @return void
+	 */
+	public function setPdfObj(resource $po): void {
 		$this->pdf_obj = $po;
 	}
 
-	public function getPdfObj() {
+	/**
+	 * Get the PDF resource object ot the generated PDF
+	 *
+	 * @return ?resource PDF resource object
+	 */
+	public function getPdfObj(): ?resource {
 		return $this->pdf_obj;
 	}
 
-	public function setPdfDir($pd) {
+	/**
+	 * Set the path where the generated PDF file should be saved
+	 *
+	 * @param string $pd Path of the file
+	 *
+	 * @return void
+	 */
+	public function setPdfDir(string $pd): void {
 		$this->pdf_dir = $pd;
 	}
 
-	public function getPdfDir() {
+	/**
+	 * Get the path where the generated PDF file should be saved
+	 *
+	 * @return string Path of the file
+	 */
+	public function getPdfDir(): string {
 		return $this->pdf_dir;
 	}
 
-	public function render() {
+	/**
+	 * Generate the PDF file
+	 *
+	 * @return void
+	 */
+	public function render(): void {
 		$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 		// set document information
@@ -147,7 +272,12 @@ class OPDF {
 		$this->setPdfObj($pdf);
 	}
 
-	public function getPdf() {
+	/**
+	 * Save generated PDF file to the previously specified path
+	 *
+	 * @return void
+	 */
+	public function getPdf(): void {
 		$this->render();
 		$pdf = $this->getPdfObj();
 		$pdf->Output($this->getPdfDir(), 'I');
