9e42b11ac679f3d3036e8a4a06902436e2f017c3
igorosabel <inigo.gorosabel@gmail.com>
2021-03-24 11:08:31 +0100

    translate - 3.0.0 - Uso de ficheros PO

--- translate/OTranslate.php
+++ translate/OTranslate.php
@@ -6,53 +6,63 @@ namespace OsumiFramework\OFW\Plugins;
  * Utility class to internationalize an application
  */
 class OTranslate {
-	private string $page         = '';
-	private ?array $data         = null;
-	private array  $translations = [];
+	private ?string $path = null;
+	private ?string $lang = null;
+	private array $headers = [];
+	private array $translations = [];
 
 	/**
-	 * Set key name of the loaded page
+	 * Set path to the PO file
 	 *
-	 * @param string $p Key name of the loaded page
+	 * @param string $path Path to the PO file
 	 *
 	 * @return void
 	 */
-	public function setPage(string $p): void {
-		$this->page = $p;
-		$this->loadTranslations();
+	public function setPath(string $path): void {
+		$this->path = $path;
 	}
 
 	/**
-	 * Get key name of the loaded page
+	 * Get path to the PO file
 	 *
-	 * @return string Key name of the loaded page
+	 * @return string Path to the PO file
 	 */
-	public function getPage(): string {
-		return $this->page;
+	public function getPath(): string {
+		return $this->path;
 	}
 
 	/**
-	 * Set whole list translation strings
+	 * Set language code of the PO file
 	 *
-	 * @param array $d List of translation strings
+	 * @param string $lang Language code of the PO file (eg: en/es/eu)
 	 *
 	 * @return void
 	 */
-	public function setData(array $d): void {
-		$this->data = $d;
+	public function setLang(string $lang): void {
+		$this->lang = $lang;
+		$this->headers['Language'] = $lang;
 	}
 
 	/**
-	 * Get whole list of translations strings
+	 * Get language code of the PO file
 	 *
-	 * @return array List of translation strings
+	 * @return string Language code of the PO file (eg: en/es/eu)
 	 */
-	public function getData(): array {
-		return $this->data;
+	public function getLang(): string {
+		return $this->lang;
 	}
 
 	/**
-	 * Set list of translations strings to be used on loaded page
+	 * Get required translation
+	 *
+	 * @return string $key Key of the required translation
+	 */
+	public function getTranslation(string $key): string {
+		return array_key_exists($key, $this->translations) ? $this->translations[$key] : null;
+	}
+
+	/**
+	 * Set list of translations strings
 	 *
 	 * @param array $t List of translation strings
 	 *
@@ -63,7 +73,7 @@ class OTranslate {
 	}
 
 	/**
-	 * Get list of translation strings to be used on loaded page
+	 * Get list of translation strings
 	 *
 	 * @return array List of translation strings
 	 */
@@ -72,21 +82,195 @@ class OTranslate {
 	}
 
 	/**
-	 * Load list of translations from the translations file and filter for the loaded page
+	 * Set list of PO file headers
+	 *
+	 * @param array $h List of headers
 	 *
 	 * @return void
 	 */
-	public function loadTranslations(): void {
-		global $core;
-		if (is_null($this->data)) {
-			$data = json_decode( file_get_contents( $core->config->getDir('app_config').'translations.json' ), true );
-			$this->data = $data;
+	public function setHeaders(array $h): void {
+		$this->headers = $h;
+	}
+
+	/**
+	 * Get list of PO file headers
+	 *
+	 * @return array List of headers
+	 */
+	public function getHeaders(): array {
+		return $this->headers;
+	}
+
+	/**
+	 * Create or edit a header
+	 *
+	 * @param string $key Name of the header
+	 *
+	 * @param string $header Value of the header
+	 *
+	 * @return void
+	 */
+	public function setHeader(string $key, string $header): void {
+		$this->headers[$key] = $header;
+	}
+
+	/**
+	 * Create or edit a translation
+	 *
+	 * @param string $key Text to be translated
+	 *
+	 * @param string $translation Translated text
+	 *
+	 * @return void
+	 */
+	public function setTranslation(string $key, string $translation): void {
+		$this->translations[$key] = $translation;
+	}
+
+	/**
+	 * Load translations from required PO file
+	 *
+	 * @param string $path Path to the PO file
+	 *
+	 * @return void
+	 */
+	public function load(string $path): void {
+		$translations = [];
+		$headers = [];
+		if (file_exists($path)) {
+			$this->path  = $path;
+			$po = file($this->path);
+			$first_msgid = array_shift($po);
+			$first_msgstr = array_shift($po);
+			$current = [];
+			$doing_keys = false;
+			$doing_translations = false;
+			foreach ($po as $i => $line) {
+				if (trim($line) === '') {
+					continue;
+				}
+				if (substr($line, 0, 1) === '#') {
+					continue;
+				}
+				if (substr($line, 0, 1) === '"') {
+					if ($doing_keys) {
+						array_push($current, trim(substr(trim($line), 1, -1)));
+					}
+					elseif ($doing_translations) {
+						array_push($translation, trim(substr(trim($line), 1, -1)));
+					}
+					else {
+						$header = explode(':', trim(substr(trim($line), 1, -1)));
+						$header[1] = str_ireplace("\\n", "", $header[1]);
+						$headers[$header[0]] = trim($header[1]);
+						if ($header[0] === 'Language') {
+							$this->lang = trim($header[1]);
+						}
+					}
+				}
+				if (substr($line, 0, 5) === 'msgid') {
+					$headers_end = true;
+					if (count($current)!=0 && count($translation)!=0) {
+						$translations[implode("\n", $current)] = implode("\n", $translation);
+					}
+					$doing_keys = true;
+					$doing_translations = false;
+					$key = trim(substr(trim(substr($line,5)), 1, -1));
+					$current = [];
+					if ($key !== '') {
+						array_push($current, $key);
+					}
+				}
+				if (substr($line, 0, 6) === 'msgstr') {
+					$doing_keys = false;
+					$doing_translations = true;
+					$value = trim(substr(trim(substr($line, 6)), 1, -1));
+					$translation = [];
+					if ($value !== '') {
+						array_push($translation, $value);
+					}
+				}
+			}
+
+			if (count($current)!=0 && count($translation)!=0) {
+				$translations[implode("\n", $current)] = implode("\n", $translation);
+			}
 		}
-		if (array_key_exists($this->page, $this->data['translations'])) {
-			$this->translations = $this->data['translations'][$this->page];
+		$this->translations = $translations;
+		$this->headers = $headers;
+	}
+
+	/**
+	 * Save current loaded translations and headers into a file
+	 *
+	 * @param string $path Path of the file
+	 *
+	 * @return bool Returns if save operation was successful or not
+	 */
+	public function save(string $path = null): bool {
+		if (is_null($path)) {
+			$path = $this->path;
 		}
-		else {
-			$this->translations = [];
+		if (is_null($path)) {
+			return false;
 		}
+
+		$str = "msgid \"\"\n";
+		$str .= "msgstr \"\"\n";
+		foreach ($this->headers as $key => $value) {
+			$str .= "\"" . $key . ": " . $value . "\"\n";
+		}
+		$str .= "\n";
+		foreach ($this->translations as $key => $value) {
+			$str .= "msgid ";
+			foreach (explode("\n", $key) as $key_part) {
+				$str .= "\"" . $key_part . "\"\n";
+			}
+			$str .= "msgstr ";
+			foreach (explode("\n", $value) as $value_part) {
+				$str .= "\"" . $value_part . "\"\n";
+			}
+			$str .= "\n";
+		}
+
+		if (file_exists($path)) {
+			unlink($path);
+		}
+		file_put_contents($path, $str);
+		return true;
+	}
+
+	/**
+	 * Create a new PO file
+	 *
+	 * @param string $path Path to the new PO file
+	 *
+	 * @param string $lang Language code of the new PO file (eg: en/es/eu)
+	 *
+	 * @return bool Returns if create operation was successful or not
+	 */
+	public function new(string $path = null, string $lang = 'en'): bool {
+		if (is_null($path)) {
+			$path = $this->path;
+		}
+		if (is_null($path)) {
+			return false;
+		}
+		$this->path = $path;
+		$this->lang = $lang;
+
+		$this->headers = [
+			'Project-Id-Version' => '',
+			'POT-Creation-Date' => '',
+			'PO-Revision-Date' => '',
+			'Last-Translator' => '',
+			'Language-Team' => '',
+			'Language' => $this->lang,
+			'MIME-Version: 1.0' => '',
+			'Content-Type: text/plain; charset=UTF-8' => '',
+			'Content-Transfer-Encoding: 8bit' => ''
+		];
+
+		return true;
 	}
 }
